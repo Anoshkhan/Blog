f1c906f7499f9f3f7a2510c96ce92b55880f3ddf
Anosh Khan <anoshkhanakb123@gmail.com>
2022-08-09 11:58:54 +0500

    welcome-p

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+//pages
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className='bg-red-500 text-center'>
-     Blog Website
+    <Router>
+    <div className='max-w-screen-md mx-auto pt-20'>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    
+    </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Home = () =>{
+    return(
+        <div className='mb-20'>
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900 '>Hello, Wecome To My Blog</h1>
+            <p className='mx-auto leading-relaxed text-base mb-4'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+            doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore 
+            veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim 
+            ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit</p>
+
+            <p className='mx-auto leading-relaxed text-base mb-4'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium 
+            doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore 
+            veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim 
+            ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit</p>
+
+            <p className='mx-auto leading-relaxed text-base mb-4'>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis 
+            praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias
+            excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui 
+            officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem
+            rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est
+            eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere 
+            possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem
+            quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et 
+            voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum
+            </p>
+        </div>
+    )}
+export default Home
