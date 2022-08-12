ed581ba55d079e21cd4f04dba54c584eb8bd9bf9
Anosh Khan <anoshkhanakb123@gmail.com>
2022-08-12 08:08:37 +0500

    addpages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 //pages
 import Home from './pages/Home';
+import About from "./pages/About";
+import ArticlesList from "./pages/ArticlesList";
+import Article from "./pages/Article";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
     <div className='max-w-screen-md mx-auto pt-20'>
     <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/articles-list" element={<ArticlesList/>} />
+    <Route path="/article" element={<Article />} />
     
     </Routes>
     </div>
---
+++ src/pages/Article.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Article = () => {
+    return(
+        <>
+            This is an article
+        </>
+    )
+};
+export default Article;
